7976608151daee06a3d914720e3187f86a6755fd
Ingrid Giany <ingridgianys@gmail.com>
2026-09-15 18:12:41 -0300

    Portal Transparência Removido Menu

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,13 +4,12 @@ export default function Header() {
   return (
     <header className="site-header">
       <div className="container">
-        <div className="brand">Condomínio Sol e Mar</div>
+        <a className="brand" href="/">Condomínio Sol e Mar</a>
         <nav>
           <a href="/#history">História</a>
           <a href="/#gallery">Galeria</a>
           <a href="/#imoveis">Imóveis</a>
           <a href="/#contacts">Contato</a>
-          <a href="/transparencia">Portal da Transparência</a>
           <a href="/login" className="btn">Área do Morador</a>
         </nav>
       </div>
